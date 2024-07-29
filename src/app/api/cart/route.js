2870180a0b1dcb4ecf299b5fa393350a9deb725e
Chris <chrisdietrich3666@gmail.com>
2024-07-29 06:43:28 -0400

    debug middleware

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -1,33 +1,42 @@
 import CartModel from "../../models/cart";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import mongoose from "mongoose";
 
-// Exported GET handler
+// export async function GET(req) {
+//   console.log("first"); // Log message to indicate the route handler is executed
+//   return new Response("Hello from the /api/cart route", {
+//     status: 200,
+//     headers: { "Content-Type": "text/plain" },
+//   });
+// }
 export const GET = async (req) => {
   // try {
-  // Retrieve userId from custom header set by middleware
   const requestHeaders = new Headers(req.headers);
   const userId = requestHeaders.get("x-decoded-id");
-  console.log("hello", userId);
+  console.log("first", userId);
 
-  // Check if userId is present
-  //   if (!userId || userId === "undefined") {
-  //     return NextResponse.json({ message: "Unauthorized user" }, { status: 401 });
-  //   }
+  if (!userId || userId === "undefined") {
+    return NextResponse.json({ message: "Unauthorized user" }, { status: 401 });
+  }
 
   //   // Convert userId to Mongoose ObjectId if necessary
   //   const objectId = mongoose.Types.ObjectId(userId);
 
   //   // Find the cart using the userId
-  //   const foundCart = await CartModel.findOne({ userId: objectId });
+  const foundCart = await CartModel.findOne({ userId });
+  console.log(foundCart);
+  //   console.log("foundCart", foundCart);
 
   //   // If no cart is found, return an empty cart or appropriate message
   //   if (!foundCart) {
-  //     return NextResponse.json({ message: "Cart not found", cart: [] }, { status: 200 });
+  //     return NextResponse.json(
+  //       { message: "Cart not found", cart: [] },
+  //       { status: 200 }
+  //     );
   //   }
 
   //   // Returning the found cart as a JSON response with a status code 200
-  //   return NextResponse.json(foundCart, { status: 200 });
+  return NextResponse.json(foundCart, { status: 200 });
   // } catch (error) {
   //   // If an error occurs, respond with the error message and a status code 500
   //   return NextResponse.json({ message: error.message }, { status: 500 });
---
+++ src/middleware.js
@@ -0,0 +1,40 @@
+import { jwtVerify } from "jose"; // Import the specific function you need from 'jose'
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/api/products", "/api/cart", "/api/cart/:path*"], // Define routes for which the middleware is applied
+};
+
+// export async function middleware(req) {
+//   console.log("hello middleware"); // This log should appear in your server console
+
+//   const response = NextResponse.next(); // Proceed to the next middleware or API route
+//   return response;
+// }
+
+export async function middleware(req) {
+  console.log("hello middleware");
+  try {
+    const authorization = req.headers.get("authorization"); // Get the Authorization header
+
+    if (!authorization) {
+      throw new Error("Token not found"); // Throw an error if Authorization header is missing
+    }
+
+    const token = authorization.split(" ")[1]; // Split the Authorization header to extract the token
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET); // Encode the JWT secret
+
+    // Verify the token and extract the payload
+    const { payload } = await jwtVerify(token, secret);
+
+    // Add userId to the request headers using the setHeader function
+    const response = NextResponse.next();
+    response.headers.set("x-decoded-id", payload.userId); // Set custom header with userId
+    
+    return response; // Proceed to the next middleware or API route
+  } catch (error) {
+    console.log(error); // Log any errors that occur during token verification
+    return NextResponse.json({ message: "unauthorized" }, { status: 401 }); // Return unauthorized response
+  }
+}

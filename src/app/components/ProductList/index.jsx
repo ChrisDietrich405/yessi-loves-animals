6ecc0cb4c4dd536f2a738032b69f2900886d8e1b
Chris <chrisdietrich3666@gmail.com>
2024-08-08 12:26:17 -0400

    test

--- src/app/components/ProductList/index.jsx
+++ src/app/components/ProductList/index.jsx
@@ -3,25 +3,25 @@ import { useState, useEffect } from "react";
 import CircularIndeterminate from "../Loader";
 import { api } from "../../api/product-api";
 import { Button, Container, Typography, Box } from "@mui/material";
-import data from "../../data.json";
+// import data from "../../data.json";
 import ProductCard from "../ProductCard";
 
 import styles from "./styles.module.css";
 
 const ProductList = () => {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // const fetchProducts = async () => {
-  //   setLoading(true);
-  //   const response = await api.get("products");
-  //   const { data } = response;
-  //   setProducts(data);
-  //   setLoading(false);
-  // };
-  // useEffect(() => {
-  //   fetchProducts();
-  // }, []);
+  const fetchProducts = async () => {
+    setLoading(true);
+    const response = await api.get("http://localhost:3000/api/products");
+    const { data } = response;
+    setProducts(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return loading ? (
     <Box
@@ -38,7 +38,7 @@ const ProductList = () => {
   ) : (
     <Box className={styles.product_list_container}>
       <Box className={styles.product_container}>
-        {data.map((productItem) => {
+        {products.map((productItem) => {
           return <ProductCard key={productItem.id} {...productItem} />;
         })}
       </Box>

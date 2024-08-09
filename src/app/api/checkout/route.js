50eb4923228179e3b1a7808d55fe2549677832aa
Chris <chrisdietrich3666@gmail.com>
2024-08-09 10:03:23 -0400

    test

---
+++ src/app/api/checkout/route.js
@@ -0,0 +1,28 @@
+// pages/api/checkout_sessions.js
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+  try {
+    const { priceId } = await request.json();
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.BASE_URL}/success`,
+      cancel_url: `${process.env.BASE_URL}/cancel`,
+    });
+
+    return NextResponse.json({ id: session.id });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
